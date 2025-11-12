0b4fe612a2c7ee387802c1a5bafad5723ba9afaf
alemen <alevalmen@alu.edu.gva.es>
2025-11-12 19:01:37 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from funciones.py import *
+
+nivelDif = 0
+while nivelDif < 1 or nivelDif > 3:
+    try:
+        nivelDif = int(input("Por favor, seleccione el nivel de dificultad: "))
+        if nivelDif < 1 or nivelDif > 3:
+            print("Por favor, introduce un valor válido: 1, 2 ó 3.")
+    except Exception as error:
+        print("Por favor, introduce un valor válido: 1, 2 ó 3.")
+        nivelDif = 0
+    
+crearTableroJugador(nivelDif)
