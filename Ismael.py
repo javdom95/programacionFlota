d9aaf169893259b659ca535bcc5c575b9b6e338f
ismgon <ismaelgg0305@gmail.com>
2025-11-12 19:20:51 +0100

    a

--- Ismael.py
+++ Ismael.py
@@ -10,8 +10,8 @@ def PideCoordenadas(tablero):
 
 def seleccionar_dificultad():
     while True:
-        opcion = input("Elige una dificultad (1-3): ")
         print("1. Fácil\n2. Media\n3. Difícil")
+        opcion = input("Elige una dificultad (1-3): ")
         if opcion in ("1", "2", "3"):
             return int(opcion)
         print("Opción no válida. Intenta de nuevo.\n")
