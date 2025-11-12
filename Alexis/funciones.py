451ca648c5ac5c32322ab2bf7e27323c653f5040
alemen <alevalmen@alu.edu.gva.es>
2025-11-12 19:26:33 +0100

    a

---
+++ Alexis/funciones.py
@@ -0,0 +1,21 @@
+def MenuDificultad():
+    while True:
+        print() 
+
+def PideCoordenadas(tablero):
+    CoordenadaX = 0
+
+
+'''
+L = 1 casilla
+B = 3 casillas horizontales
+Z = 4 casillas horizontales consecutivas
+P = 5 verticules
+nivel 1 10 barcos 5L, 3B, 1Z, 1P.
+nivel 2 5 barcos 2L, 1B, 1Z, 1P.
+nivel 3 2 barcos 1L, 1B
+''' [3][3]
+def crearTableroJugador(dificultad):
+    if dificultad == 1:
+
+    if dificultad
--- funciones.py
+++ funciones.py
@@ -1,21 +1,6 @@
 def MenuDificultad():
     while True:
-        print()
+        print() 
 
 def PideCoordenadas(tablero):
     CoordenadaX = 0
-
-
-'''
-L = 1 casilla
-B = 3 casillas horizontales
-Z = 4 casillas horizontales consecutivas
-P = 5 verticules
-nivel 1 10 barcos 5L, 3B, 1Z, 1P.
-nivel 2 5 barcos 2L, 1B, 1Z, 1P.
-nivel 3 2 barcos 1L, 1B
-'''
-def crearTableroJugador(dificultad):
-    if dificultad == 1:
-
-    if dificultad
