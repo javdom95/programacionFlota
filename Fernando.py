bf3e16b4d588268981e2f4e2ce77f349fa29c473
Fernis23227 <fernanru07@gmail.com>
2025-11-13 16:05:29 +0100

    Update Fernando.py

--- Fernando.py
+++ Fernando.py
@@ -1,55 +1,55 @@
-def PideCoordenadas(tablero):
+def PideCoordenadas(array):
     try:
         while True:
             fila = str(input("Introduce una fila a elegir (A-J):"))
             fila.upper()
-            columna = int(input("Introduce una columna a elegir (0-9)"))
-            if (fila != "A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J") or columna > 9 or columna < 0:
+            col = int(input("Introduce una col a elegir (0-9)"))
+            if (fila != "A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J") or col > 9 or col < 0:
                 print("Coordenadas incorrectas, vuelve a introducir las coordenadas porfavor")
             elif fila == "A":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 0
             elif fila == "B":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 1
             elif fila == "C":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 2
             elif fila == "D":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 3
             elif fila == "E":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 4
             elif fila == "F":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 5
             elif fila == "G":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 6
             elif fila == "H":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 7
             elif fila == "I":
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 8
             else:
-                print(f"Coordenada x = {fila} y = {columna}")
+                print(f"Coordenada x = {fila} y = {col}")
                 fila = 9
-            return fila, columna
+            return fila, col
     except ValueError:
         print("Valor incorrecto, vuelve a intertarlo")
 
-def ComprobarCoordenadas(fila,columna,tablero):
-    tableroObjetivo = tablero[fila][columna]
-    return tableroObjetivo
+def ComprobarCoordenadas(fila,col,arrayOrdenador):
+    arrayObjetivo = arrayOrdenador[fila][col]
+    return arrayObjetivo
 
-def RealizarDisparo(tableroObjetivo,tablero,fila,columna):
-    if tableroObjetivo == "P" or "Z" or "B" or "L":
-        tablero[fila][columna] = X
+def RealizarDisparo(arrayObjetivo,arrayOrdenador,fila,col):
+    if arrayObjetivo == "P" or "Z" or "B" or "L":
+        arrayOrdenador[fila][col] = "X"
         print("Tocado")
         return True
     else:
-        tablero[fila][columna] = X
+        arrayOrdenador[fila][col] = "A"
         print("Agua")
         return False
